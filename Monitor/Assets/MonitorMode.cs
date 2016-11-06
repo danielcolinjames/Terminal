4498df1979c7e9d11edad94e95f8c4458e4dc6c5
danielcolinjames <danielcolinjames@gmail.com>
2016-11-06 11:18:02 -0500

    Calculated distance between player and monitor
    Got a number to print out based on the distance between the player and
    the monitor. This will be used to activate monitor mode.

---
+++ Monitor/Assets/MonitorMode.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonitorMode : MonoBehaviour {
+
+    public Transform player;
+    public Transform monitor;
+    public float distanceToMonitor;
+
+    void Awake() {
+        player = GameObject.Find("FPSController").transform;
+        monitor = GameObject.Find("Monitor").transform;
+
+        distanceToMonitor = 0;
+    }
+    
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        distanceToMonitor = Vector3.Distance(player.position, monitor.position);
+        print(distanceToMonitor);
+	}
+}
