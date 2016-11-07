f64f5e52180e535f221b8dc49c08c57816030d30
danielcolinjames <danielcolinjames@gmail.com>
2016-11-06 23:07:15 -0500

    Added basic controller functionality
    Imported XInputInterface plugin. Needs lots of tweaking but it reads
    Xbox controller input easily.

--- Monitor/Assets/MonitorMode.cs
+++ Monitor/Assets/MonitorMode.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using XInputDotNetPure;
 
 public class MonitorMode : MonoBehaviour {
 
     public Transform player;
     public Transform monitor;
     public float distanceToMonitor;
+
+    bool playerIndexSet = false;
+    PlayerIndex playerIndex;
+    GamePadState state;
+    GamePadState prevState;
+
+    bool monitorMode = false;
+    bool aPressed = false;
 
     void Awake() {
         player = GameObject.Find("FPSController").transform;
@@ -23,6 +32,61 @@ public class MonitorMode : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         distanceToMonitor = Vector3.Distance(player.position, monitor.position);
-        print(distanceToMonitor);
-	}
+       // print(distanceToMonitor);
+
+
+        if (!playerIndexSet || !prevState.IsConnected)
+        {
+            for (int i = 0; i < 4; ++i)
+            {
+                PlayerIndex testPlayerIndex = (PlayerIndex)i;
+                GamePadState testState = GamePad.GetState(testPlayerIndex);
+                if (testState.IsConnected)
+                {
+                    Debug.Log(string.Format("GamePad found {0}", testPlayerIndex));
+                    playerIndex = testPlayerIndex;
+                    playerIndexSet = true;
+                }
+            }
+        }
+
+
+        prevState = state;
+        state = GamePad.GetState(playerIndex);
+        
+
+        // Detect if a button was pressed this frame
+        if (prevState.Buttons.A == ButtonState.Released && state.Buttons.A == ButtonState.Pressed)
+        {
+            // print("a pressed");
+            aPressed = true;
+            print("YEAH");
+        }
+
+        // Detect if a button was released this frame
+        if (prevState.Buttons.A == ButtonState.Pressed && state.Buttons.A == ButtonState.Released)
+        {
+            //   print("a released");
+
+        }
+
+        // if a has been pressed, activate monitor mode
+        if (aPressed == true && distanceToMonitor < 1.5)
+        {
+            monitorMode = true;
+        }
+
+        if (monitorMode == true)
+        {
+            print("mm");
+            player.position = new Vector3((float)-1.5,(float)1,(float)0);
+        }
+
+        // Set vibration according to triggers
+        //GamePad.SetVibration(playerIndex, state.Triggers.Left, state.Triggers.Right);
+
+        // Make the current object turn
+        //transform.localRotation *= Quaternion.Euler(0.0f, state.ThumbSticks.Left.X * 25.0f * Time.deltaTime, 0.0f);
+
+    }
 }
--- Monitor/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ Monitor/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -156,7 +156,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
 
             m_NextStep = m_StepCycle + m_StepInterval;
 
-            PlayFootStepAudio();
+           // PlayFootStepAudio();
         }
 
 
