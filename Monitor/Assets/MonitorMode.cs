e40211c1ba84e17ed5508b61a985986ae53e5e4f
danielcolinjames <danielcolinjames@gmail.com>
2016-11-08 00:53:01 -0500

    Added monitor mode and on-screen cube movement
    Pressing A while standing in front of the monitor now locks the player
    in front of the monitor, and when in this mode, the left joystick
    controls the on-screen cube's X and Y position.

--- Monitor/Assets/MonitorMode.cs
+++ Monitor/Assets/MonitorMode.cs
@@ -8,6 +8,8 @@ public class MonitorMode : MonoBehaviour {
     public Transform monitor;
     public float distanceToMonitor;
 
+    public Transform box;
+
     bool playerIndexSet = false;
     PlayerIndex playerIndex;
     GamePadState state;
@@ -19,6 +21,8 @@ public class MonitorMode : MonoBehaviour {
     void Awake() {
         player = GameObject.Find("FPSController").transform;
         monitor = GameObject.Find("Monitor").transform;
+
+        box = GameObject.Find("PuzzleOneCube").transform;
 
         distanceToMonitor = 0;
     }
@@ -50,36 +54,37 @@ public class MonitorMode : MonoBehaviour {
             }
         }
 
-
         prevState = state;
         state = GamePad.GetState(playerIndex);
-        
 
         // Detect if a button was pressed this frame
         if (prevState.Buttons.A == ButtonState.Released && state.Buttons.A == ButtonState.Pressed)
         {
             // print("a pressed");
-            aPressed = true;
-            print("YEAH");
+
+            // if a has been pressed, activate monitor mode
+            if (distanceToMonitor < 1.5)
+            {
+                monitorMode = true;
+            }
         }
 
         // Detect if a button was released this frame
         if (prevState.Buttons.A == ButtonState.Pressed && state.Buttons.A == ButtonState.Released)
         {
             //   print("a released");
-
         }
-
-        // if a has been pressed, activate monitor mode
-        if (aPressed == true && distanceToMonitor < 1.5)
-        {
-            monitorMode = true;
-        }
+        
 
         if (monitorMode == true)
         {
-            print("mm");
-            player.position = new Vector3((float)-1.5,(float)1,(float)0);
+            player.position = new Vector3((float)-1.5,(float)1.05,(float)0);
+
+            box.Translate(Vector3.back * state.ThumbSticks.Left.Y);
+            box.Translate(Vector3.forward * -state.ThumbSticks.Left.Y);
+
+            box.Translate(Vector3.right * state.ThumbSticks.Left.X);
+            box.Translate(Vector3.left * -state.ThumbSticks.Left.X);
         }
 
         // Set vibration according to triggers
@@ -88,5 +93,8 @@ public class MonitorMode : MonoBehaviour {
         // Make the current object turn
         //transform.localRotation *= Quaternion.Euler(0.0f, state.ThumbSticks.Left.X * 25.0f * Time.deltaTime, 0.0f);
 
+        
+
+        // box.Translate(Vector3.back * Time.deltaTime);
     }
 }
