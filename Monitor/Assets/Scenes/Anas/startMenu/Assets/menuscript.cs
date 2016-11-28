2f1c204b577124eb9dfe506b1666de59961369b4
tossedwarrior <anas.ashraf@live.com>
2016-11-27 19:46:35 -0500

    Start menu is done

---
+++ Monitor/Assets/Scenes/Anas/startMenu/Assets/menuscript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class menuscript : MonoBehaviour
+{
+
+    public Canvas exitMenu;
+    public Button Play;
+    public Button Exit;
+
+    // Use this for initialization
+    void Start()
+    {
+        exitMenu = exitMenu.GetComponent<Canvas>();
+        Play = Play.GetComponent<Button>();
+        Exit = Exit.GetComponent<Button>();
+        exitMenu.enabled = false;
+    }
+
+    public void ExitPress()
+    {
+        exitMenu.enabled = true;
+        Play.enabled = false;
+        Exit.enabled = false;
+    }
+
+    public void NoPress()
+    {
+        exitMenu.enabled = false;
+        Play.enabled = true;
+        Exit.enabled = true;
+    }
+
+    public void StartLevel()
+    {
+        SceneManager.LoadScene("Menu_Level");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
