1938e42d816b2d5bd6f16a8790a14d10450ca647
danielcolinjames <danielcolinjames@gmail.com>
2016-11-28 01:30:08 -0500

    Added controller functionality for object moving
    PuzzleTwo.cs now counts the number of cubes in each colour light, but
    the position isn't working for some reason.

--- Monitor/Assets/Scripts/PuzzleTwo.cs
+++ Monitor/Assets/Scripts/PuzzleTwo.cs
@@ -10,29 +10,44 @@ public class PuzzleTwo : MonoBehaviour
 
     // Warehouse items
     public Light yellowLight;
-    public bool yellowFlag;
+    public int yellowCount;
+    public Transform yellowGoal;
     public Transform yellowBoxOne;
+    float yellowBoxOneDistance;
     public Transform yellowBoxTwo;
+    float yellowBoxTwoDistance;
     public Transform yellowBoxThree;
+    float yellowBoxThreeDistance;
 
     public Light redLight;
-    public bool redFlag;
+    public int redCount;
+    public Transform redGoal;
     public Transform redBoxOne;
+    float redBoxOneDistance;
     public Transform redBoxTwo;
+    float redBoxTwoDistance;
     public Transform redBoxThree;
+    float redBoxThreeDistance;
 
     public Light greenLight;
-    public bool greenFlag;
+    public int greenCount;
+    public Transform greenGoal;
     public Transform greenBoxOne;
+    float greenBoxOneDistance;
     public Transform greenBoxTwo;
+    float greenBoxTwoDistance;
     public Transform greenBoxThree;
+    float greenBoxThreeDistance;
 
     public Light blueLight;
-    public bool blueFlag;
+    public int blueCount;
+    public Transform blueGoal;
     public Transform blueBoxOne;
+    float blueBoxOneDistance;
     public Transform blueBoxTwo;
+    float blueBoxTwoDistance;
     public Transform blueBoxThree;
-
+    float blueBoxThreeDistance;
 
     // Monitor items
     public Transform puzzleTwoRedBox;
@@ -52,6 +67,7 @@ public class PuzzleTwo : MonoBehaviour
     public float distanceFromBlueX;
     public float distanceFromBlueZ;
 
+    public float acceptableDistance = 0.5f;
 
     public Transform puzzleTwoSelector;
 
@@ -65,6 +81,28 @@ public class PuzzleTwo : MonoBehaviour
         puzzleTwoGreenBox = GameObject.FindGameObjectWithTag("PuzzleTwoGreenBox").transform;
         puzzleTwoYellowBox = GameObject.FindGameObjectWithTag("PuzzleTwoYellowBox").transform;
 
+        redGoal = GameObject.FindGameObjectWithTag("RedGoal").transform;
+        blueGoal = GameObject.FindGameObjectWithTag("BlueGoal").transform;
+        greenGoal = GameObject.FindGameObjectWithTag("GreenGoal").transform;
+        yellowGoal = GameObject.FindGameObjectWithTag("YellowGoal").transform;
+
+        redBoxOne = GameObject.FindGameObjectWithTag("RedBoxOne").transform;
+        redBoxTwo = GameObject.FindGameObjectWithTag("RedBoxTwo").transform;
+        redBoxThree = GameObject.FindGameObjectWithTag("RedBoxThree").transform;
+
+        blueBoxOne = GameObject.FindGameObjectWithTag("BlueBoxOne").transform;
+        blueBoxTwo = GameObject.FindGameObjectWithTag("BlueBoxTwo").transform;
+        blueBoxThree = GameObject.FindGameObjectWithTag("BlueBoxThree").transform;
+
+        greenBoxOne = GameObject.FindGameObjectWithTag("GreenBoxOne").transform;
+        greenBoxTwo = GameObject.FindGameObjectWithTag("GreenBoxTwo").transform;
+        greenBoxThree = GameObject.FindGameObjectWithTag("GreenBoxThree").transform;
+
+        yellowBoxOne = GameObject.FindGameObjectWithTag("YellowBoxOne").transform;
+        yellowBoxTwo = GameObject.FindGameObjectWithTag("YellowBoxTwo").transform;
+        yellowBoxThree = GameObject.FindGameObjectWithTag("YellowBoxThree").transform;
+
+        // public Transform flashlight = GameObject.FindGameObjectWithTag("Flashlight").GetComponent<Light>();
         yellowLight = GameObject.FindGameObjectWithTag("YellowLight").GetComponent<Light>();
         redLight = GameObject.FindGameObjectWithTag("RedLight").GetComponent<Light>();
         greenLight = GameObject.FindGameObjectWithTag("GreenLight").GetComponent<Light>();
@@ -119,9 +157,10 @@ public class PuzzleTwo : MonoBehaviour
             {
                 float movementSpeed = 0.05f;
 
-                // locks rotation of box
+                // locks rotation of light
                 puzzleTwoSelector.transform.rotation = Quaternion.Euler(puzzleTwoSelector.transform.rotation.eulerAngles.x, Global.lockPos, Global.lockPos);
 
+                // TODO: limits, eg: if (pos.x >) { ... }
                 // up and down
                 puzzleTwoSelector.Translate(Vector3.down * Global.state.ThumbSticks.Left.Y * movementSpeed);
                 puzzleTwoSelector.Translate(Vector3.up * -Global.state.ThumbSticks.Left.Y * movementSpeed);
@@ -130,16 +169,81 @@ public class PuzzleTwo : MonoBehaviour
                 puzzleTwoSelector.Translate(Vector3.right * -Global.state.ThumbSticks.Left.X * movementSpeed);
                 puzzleTwoSelector.Translate(Vector3.left * Global.state.ThumbSticks.Left.X * movementSpeed);
 
-                blueLight.enabled = (distanceFromBlueX < 0.5 && distanceFromBlueZ < 0.5);
-                redLight.enabled = (distanceFromRedX < 0.5 && distanceFromRedZ < 0.5);
-                greenLight.enabled = (distanceFromGreenX < 0.5 && distanceFromGreenZ < 0.5);
-                yellowLight.enabled = (distanceFromYellowX < 0.5 && distanceFromYellowZ < 0.5);
 
-                // 
-                yellowFlag = false;
-                blueFlag = false;
-                redFlag = false;
-                greenFlag = false;
+
+                blueLight.enabled = (distanceFromBlueX < acceptableDistance && distanceFromBlueZ < acceptableDistance);
+                //     true; blueFlag = true;
+                //} else {
+                //    blueLight.enabled = false; blueFlag = false;
+                //}
+
+                redLight.enabled = (distanceFromRedX < acceptableDistance && distanceFromRedZ < acceptableDistance);
+                //    redLight.enabled = true; redFlag = true;
+                //} else {
+                //    redLight.enabled = false; redFlag = false;
+                //}
+
+                greenLight.enabled = (distanceFromGreenX < acceptableDistance && distanceFromGreenZ < acceptableDistance);
+                //    greenLight.enabled = true; greenFlag = true;
+                //} else {
+                //    greenLight.enabled = false; greenFlag = false;
+                //}
+
+                yellowLight.enabled = (distanceFromYellowX < acceptableDistance && distanceFromYellowZ < acceptableDistance);
+                //    yellowLight.enabled = true; yellowFlag = true;
+                //}
+                //else {
+                //    yellowLight.enabled = false; yellowFlag = false;
+                //}
+
+
+                redBoxOneDistance = Vector3.Distance(redBoxOne.position, redGoal.position);
+                redBoxTwoDistance = Vector3.Distance(redBoxTwo.position, redGoal.position);
+                redBoxThreeDistance = Vector3.Distance(redBoxThree.position, redGoal.position);
+
+                blueBoxOneDistance = Vector3.Distance(blueBoxOne.position, blueGoal.position);
+                blueBoxTwoDistance = Vector3.Distance(blueBoxTwo.position, blueGoal.position);
+                blueBoxThreeDistance = Vector3.Distance(blueBoxThree.position, blueGoal.position);
+
+                greenBoxOneDistance = Vector3.Distance(greenBoxOne.position, greenGoal.position);
+                greenBoxTwoDistance = Vector3.Distance(greenBoxTwo.position, greenGoal.position);
+                greenBoxThreeDistance = Vector3.Distance(greenBoxThree.position, greenGoal.position);
+
+                yellowBoxOneDistance = Vector3.Distance(yellowBoxOne.position, yellowGoal.position);
+                yellowBoxTwoDistance = Vector3.Distance(yellowBoxTwo.position, yellowGoal.position);
+                yellowBoxThreeDistance = Vector3.Distance(yellowBoxThree.position, yellowGoal.position);
+
+                print(yellowBoxTwo.position);
+
+                if (blueLight.enabled) {
+                    blueCount = 0;
+                    if (blueBoxOneDistance < 0.5) blueCount++;
+                    if (blueBoxTwoDistance < 0.5) blueCount++;
+                    if (blueBoxThreeDistance < 0.5) blueCount++;
+                }
+                if (redLight.enabled) {
+                    redCount = 0;
+                    if (redBoxOneDistance < 0.5) redCount++;
+                    if (redBoxTwoDistance < 0.5) redCount++;
+                    if (redBoxThreeDistance < 0.5) redCount++;
+                }
+                if (greenLight.enabled) {
+                    greenCount = 0;
+                    if (greenBoxOneDistance < 0.5) greenCount++;
+                    if (greenBoxTwoDistance < 0.5) greenCount++;
+                    if (greenBoxThreeDistance < 0.5) greenCount++;
+                }
+                if (yellowLight.enabled) {
+                    yellowCount = 0;
+                    if (yellowBoxOneDistance < 0.5) yellowCount++;
+                    if (yellowBoxTwoDistance < 0.5) yellowCount++;
+                    if (yellowBoxThreeDistance < 0.5) yellowCount++;
+                }
+
+                //puzzleTwoYellowBox.localScale -= new Vector3(0.5f * yellowCount, 0.5f * yellowCount, 0.5f * yellowCount);
+                print("Yellow = " + yellowCount);
+                print("BOX TWO DIST: " + yellowBoxTwoDistance);
+                //puzzleTwoYellowBox.localScale -= new Vector3(6, 8, 9);
 
                 // TODO box distance to goal
                 //puzzleOneDistanceToCompletion = Vector3.Distance(puzzleOneBox.position, puzzleOneComplete.position);
