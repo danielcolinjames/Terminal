a70d286916ca63c18ca2d68d85e0fd8651573e2f
teriaki <mike.scot.stuart@gmail.com>
2016-11-29 09:14:41 -0500

    ALPHA VERSON v 0.2.0
    small tweaks for build

---
+++ Monitor/Assets/Scripts/ToastLightScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class ToastLightScript : MonoBehaviour {
+    public Light flashlight;
+    // Use this for initialization
+    public KeyCode input = KeyCode.T;
+
+    void Awake()
+    {
+        flashlight = GameObject.FindGameObjectWithTag("ToastLight").GetComponent<Light>();
+
+    }
+
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if ((Input.GetKeyDown(input)) || (Global.prevState.DPad.Right == ButtonState.Released && Global.state.DPad.Right == ButtonState.Pressed))
+        {
+            flashlight.enabled = !flashlight.enabled;
+        }
+    }
+}
